da5ae6def246a60dce98c86b6b2f32f74ef8d807
cyril-ui-developer <cyril.ajieh@gmail.com>
2026-09-06 21:34:50 -0400

    add struct

--- main.go
+++ main.go
@@ -10,6 +10,13 @@ import (
 	"go-log-parser/greeting"
 )
 
+type parser struct {
+		sum map[string]int
+	    domains []string
+		total int
+		lines int
+}
+
 func main() {
 	fmt.Println(greeting.Message())
 	
@@ -18,50 +25,42 @@ func main() {
 		return
 	}
 
-	var (
-		sum map[string]int
-		domains []string
-		total int
-		lines int
-	)
+	p := parser{sum: make(map[string]int)}
 
 	in := bufio.NewScanner(os.Stdin)
 
 	for in.Scan() {
-		lines++
+		p.lines++
 
 		fields := strings.Fields(in.Text())
 		if len(fields) != 2 {
-			fmt.Fprintf(os.Stderr, "invalid input: %q(line #%d)\n", in.Text(), lines)
+			fmt.Fprintf(os.Stderr, "invalid input: %q(line #%d)\n", in.Text(), p.lines)
 			return
 		}
 
 		domain := fields[0]
 		visits, err := strconv.Atoi(fields[1])
 		if visits < 0 || err != nil {
-			fmt.Fprintf(os.Stderr, "invalid number of visits: %q(line #%d)\n", fields[1], lines)
+			fmt.Fprintf(os.Stderr, "invalid number of visits: %q(line #%d)\n", fields[1], p.lines)
 			return
 		}
 
-		if sum == nil {
-			sum = make(map[string]int)
-		}
 		
-		if _, ok := sum[domain]; !ok {
-			domains = append(domains, domain)
+		if _, ok := p.sum[domain]; !ok {
+			p.domains = append(p.domains, domain)
 		}
-		total += visits	
-		sum[domain] += visits
+		p.total += visits	
+		p.sum[domain] += visits
 	}
 
 	fmt.Printf("%-30s %10s\n", "Domain", "visit counts:")
 
-	sort.Strings(domains)
-	for _, domain := range domains {
-		visits := sum[domain]
+	sort.Strings(p.domains)
+	for _, domain := range p.domains {
+		visits := p.sum[domain]
 		fmt.Printf("%-30s %10d\n", domain, visits)
 	}
-	fmt.Printf("\n%-30s %10d\n", "Total", total)
+	fmt.Printf("\n%-30s %10d\n", "Total", p.total)
 
 
 if err := in.Err(); err != nil {
